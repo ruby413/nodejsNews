af20d0cb408316bf80ee605e18fde5a5c7391ca8
Ruby Lee <ruby413@hanmail.net>
2019-07-27 01:08:17 +0900

    feat : logout with express method

--- newsApp/routes/auth.js
+++ newsApp/routes/auth.js
@@ -6,6 +6,13 @@ router.post('/login', (req, res) => {
   console.log(req.sessionID)
 });
 
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.session.destroy((err) => {
+        res.redirect('/')
+    })
+})
+
 router.get('/join', (req, res) => {
     console.log("joinPage")
     res.render('join', {title : 'signUp page'})
